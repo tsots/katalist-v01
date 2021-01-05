8e0575d7c0f47233e0ac3ad408472af1703abb30
tsots <mosalatsotetsi@gmail.com>
2021-01-05 18:21:57 +0200

    first folders setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Design from './components/pages/Design'
+import Contact from './components/pages/Contact'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/design" component={Design} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/pages/Design.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Design = () => {
+    return (
+        <>
+        <h1>This is the Design Page</h1>
+        </>
+    )
+}
+
+export default Design
